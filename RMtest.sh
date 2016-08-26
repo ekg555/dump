30b4e3bafb0642f8e7cfaada3e09165ec30e86a1
ekg555 <eugene.konagaya@gmail.com>
2016-08-26 01:13:12 -0700

    Delete RMtest.sh

--- RMtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-Rscript --verbose RMtest.r
-echo MAILING
-printf "RMtest.r finished\n$(date)" | mail -r ekonagay@stanford.edu -s "RMtest.r fin" ekonagaya@berkeley.edu
-echo EXITING SCRIPT
-
-exit 1
