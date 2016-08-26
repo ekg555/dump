ec30f8ee2cb92c38e4e7a1a68f9019a2258ffc36
ekg555 <eugene.konagaya@gmail.com>
2016-08-26 00:48:26 -0700

    Update RM.r

--- RM.r
+++ RM.r
@@ -6,47 +6,40 @@ file.create("RM.Rout")
 zz <- file("/work/project/LOGS/RM.Rout", open = "wt")
 sink(zz)
 
+library(ReadMe)
+
 t_0 = date()
 
 # ===========================================================
 #  RM.R
+#  UPDATED: 2016/08/26
 # ===========================================================
-# creates "inventory" data-frame
+#  DEFINES "readem [sic]" function that sequentially makes readme calls
+#           at different-levels (state || state/yr)
 #
-# Variables:
-# ----------------
-# - state
-# - yr
-# - filepath
-# - Truth 
-# - TraningSet
-# 
-#
-#
-#
+#  REQUIRES (1) 'inventory.csv' file in a 'LOGS' folder
+#                (a full inventory of all files that could be read by readme)
+#           (2) some training set '_control.csv'
+#                
 
-
-library(ReadMe)
 
 #  LOAD Inventory & TrainingSet(s)
 # ==========================================================
 
-library(ReadMe)
-
 trnset0 <- "/work/project/control/oldcontrol/_control.csv"
+finaldir <- "/work/project/final"
 
 ctrl <- read.csv(trnset0, header=TRUE)
 inventory <- read.csv("/work/project/LOGS/inventory.csv", header=TRUE)
 indices <- c('state', 'yr')
 
-finaldir <- "/work/project/final"
 
 #  READEM: takes vector of states (and years)
-#  if given years: runs state/yr stratified readme calls
-#  if no years: runs state-stratified readme calls
+#    if given years: runs state/yr stratified readme calls
+#    if no years: runs state-stratified readme calls
 # ==========================================================
 
-readem <- function (state=c(), yr=c(), boot=FALSE) {
+readem <- function (state=c(), yr=c(), boot=TRUE) {
   noyrs <- data.frame(state=c(),yr=c())
   
   for (i in 1:length(state)) {
@@ -61,14 +54,18 @@ readem <- function (state=c(), yr=c(), boot=FALSE) {
           
           t_styr_0 <- date()
           
-          setwd( file.path(finaldir,state[i],yr[j]) )
           styr.inv <- inventory[inventory$state==state[i] & inventory$yr==yr[j],
                                 !(names(inventory) %in% indices)]
 	        styr.inv$trainingset <- 0
           styr.inv <- rbind(ctrl, styr.inv)
+          
+          # for file-label purposes...
           styr <- paste(state[i],yr[j], sep="")
           
           setwd('/work/project/LOGS')
+          write.csv(styr.inv, file=paste(styr,"_control.csv", sep=""), row.names=FALSE, quote=FALSE)
+          
+          setwd( file.path(finaldir,state[i],yr[j]) )
           write.csv(styr.inv, file=paste(styr,"_control.csv", sep=""), row.names=FALSE, quote=FALSE)
           
           # underCA2015overall.results
@@ -78,23 +75,32 @@ readem <- function (state=c(), yr=c(), boot=FALSE) {
           # undergradCA2015overall.preprocess
           assign(paste("undergrad",styr,"overall.preprocess", sep=""),
                  preprocess(get( paste("under",styr,"overall.results", sep="")) ) )
+          
           # readmeCA2015overall.results
           assign(paste("readme",styr,"overall.results", sep=""),
                  readme( get( paste("undergrad",styr,"overall.preprocess", sep="")),
                          boot.se=boot) )
         
-          # save: CA2015overall.Rdata
+          # save: CA2015overall.Rdata (in ST/YR & LOGS folder)
+          print("--------------------------------------------")
+          print(paste(styr," (n = ", nrow(styr.inv)-nrow(ctrl),")") )
+          print("-------------------------------------")
+          
           save(list= c(paste("readme",styr,"overall.results", sep=""), "noyrs"),
                file= paste(styr,"overall.Rdata",sep="") )
+          print(paste("FILE SAVED: ",getwd(),"/",styr,"overall.Rdata", sep=""))
           
-          t_styr_f <- date()
+          setwd( file.path(finaldir,state[i],yr[j]) )
+          save(list= c(paste("readme",styr,"overall.results", sep=""), "noyrs"),
+               file= paste(styr,"overall.Rdata",sep="") )
+          print(paste("FILE SAVED: ",getwd(),"/",styr,"overall.Rdata", sep=""))
           
           print("--------------------------------------------")
+          t_styr_f <- date()
           print(paste("RM state (",styr,")"," start: ", t_styr_0, sep=""))
           print(paste("RM state (",styr,")","   end: ", t_styr_f, sep=""))
-          print("-------------------------------------")
-          print(paste("FILE SAVED: ",getwd(),"/",styr,"overall.Rdata", sep=""))
           print("--------------------------------------------")
+         
         }
       }
       
@@ -104,47 +110,60 @@ readem <- function (state=c(), yr=c(), boot=FALSE) {
       yr  <- list.files()
       
       for (j in 1:length(yr)) {
+        
         setwd( file.path(finaldir,state[i],yr[j]) )
-        styr.inv <- inventory[inventory$state==state[i] & inventory$yr==yr[j],
+        st.inv <- inventory[inventory$state==state[i],
                               !(names(inventory) %in% indices)]
-	styr.inv$trainingset <- 0
-	# styr.inv[is.na(styr.inv)] <- ""
-        st.inv <- rbind(st.inv, styr.inv)
+	      st.inv$trainingset <- 0
+	      st.inv <- rbind(ctrl, st.inv)
+	      
       }
+      
       t_st_0 <- date()
       
       setwd('/work/project/LOGS')
       write.csv(st.inv, file=paste(state[i],"_control.csv", sep=""), row.names=FALSE, quote=FALSE )
      
-       # underCAoverall.results
+      setwd( file.path(finaldir,state[i]) )
+      write.csv(st.inv, file=paste(state[i],"_control.csv", sep=""), row.names=FALSE, quote=FALSE )
+      
+      # underCAoverall.results
       assign(paste("under",state[i],"overall.results", sep=""),
              undergrad(control=paste(state[i],"_control.csv", sep=""), sep=",") )
       
       # undergradCAoverall.preprocess
       assign(paste("undergrad",state[i],"overall.preprocess", sep=""),
              preprocess(get( paste("under",state[i],"overall.results", sep="")) ) )
+      
       # readmeCAoverall.results
       assign(paste("readme",state[i],"overall.results", sep=""),
              readme( get( paste("undergrad",state[i],"overall.preprocess", sep="")),
                      boot.se=boot) )
       
-      # save: CAoverall.Rdata
+      # save: CAoverall.Rdata (in ST & LOGS folder)
+      print("--------------------------------------------")
+      print(paste(state[i]," (n = ", nrow(st.inv)-nrow(ctrl),")") )
+      print("-------------------------------------")
+      
       save(list= c(paste("readme",state[i],"overall.results", sep=""), "noyrs"),
            file= paste(state[i],"overall.Rdata",sep="") )
+      print(paste("FILE SAVED: ",getwd(),"/",st,"overall.Rdata", sep=""))
       
-      t_st_f <- date()
+      setwd( file.path(finaldir,state[i]) )
+      save(list= c(paste("readme",state[i],"overall.results", sep=""), "noyrs"),
+           file= paste(state[i],"overall.Rdata",sep="") )
+      print(paste("FILE SAVED: ",getwd(),"/",st,"overall.Rdata", sep=""))
       
       print("--------------------------------------------")
-      print(paste("RM state (",st,")"," start: ", t_st_0, sep=""))
-      print(paste("RM state (",st,")","   end: ", t_st_f, sep=""))
-      print("-------------------------------------")
-      print(paste("FILE SAVED: ",getwd(),"/",st,"overall.Rdata", sep=""))
+      t_st_f <- date()
+      print(paste("RM state (",state[i],")"," start: ", t_st_0, sep=""))
+      print(paste("RM state (",state[i],")","   end: ", t_st_f, sep=""))
       print("--------------------------------------------")
     }
   }
 }
 
-# Readem Runs
+#  Readem Runs
 # ==========================================================
 
 
@@ -173,9 +192,7 @@ try( readem(state=st, yr=yrs, boot=TRUE) )
 t_styr_f <- date()
 
 
-
-
-# SAVE AND LOG TIMES
+#  SAVE AND LOG GLOBAL START/END TIMES
 # ================================================
 setwd("/work/project/LOGS")
 
